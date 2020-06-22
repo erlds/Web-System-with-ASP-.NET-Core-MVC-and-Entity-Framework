c375d2b30bd34cb7a776b39c81c746eadcf47f9c
Evaristo <evaristo.ra@hotmail.com>
2020-06-22 13:07:13 -0300

    Seeding Service

--- Startup.cs
+++ Startup.cs
@@ -39,14 +39,17 @@ namespace Ex1
             services.AddDbContext<Ex1Context>(options =>
             options.UseMySql(Configuration.GetConnectionString("Ex1Context"), builder =>
              builder.MigrationsAssembly("Ex1")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
