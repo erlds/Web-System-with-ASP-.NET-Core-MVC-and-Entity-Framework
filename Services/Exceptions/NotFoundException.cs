fb7f2b54b8226583d90d968453e4c9c92af51848
Evaristo <evaristo.ra@hotmail.com>
2020-06-25 11:30:57 -0300

    Update seller and custom service exception

---
+++ Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Security.Policy;
+
+namespace Ex1.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+
+    }
+}
