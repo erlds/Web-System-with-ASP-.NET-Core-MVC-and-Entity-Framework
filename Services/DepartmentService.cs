117912940bfb5c93f4843842079494890e93ed59
Evaristo <evaristo.ra@hotmail.com>
2020-06-25 13:52:16 -0300

    Asynchronous operations using Tasks (async, await)

--- Services/DepartmentService.cs
+++ Services/DepartmentService.cs
@@ -3,7 +3,8 @@ using Ex1.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ex1.Services
 {
@@ -16,9 +17,10 @@ namespace Ex1.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
+
     }
 }
